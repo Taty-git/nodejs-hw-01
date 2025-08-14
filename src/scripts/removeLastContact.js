78d65d3a4aabe37aca60fe2ed62534899f2fe9a6
Татьяна Татьяна <taty.bystrova@gail.com>
2025-08-14 21:26:52 +0300

    home work

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import {writeContacts} from '../utils/writeContacts.js';
+import {readContacts} from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+    try {
+        const data = await readContacts();
+        if (!data.length) return;
+        data.pop();
+
+        await writeContacts(data);
+        console.log(`Видалено останній контакт. Залишилось: ${data.length}`);
+    } catch (error){
+        console.error('Сталася помилка під час усунення контакту:', error);
+    }
+ };
 
 removeLastContact();
